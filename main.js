f4756e42711529ff6bf07380bd1d70c12c833a53
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2021-11-14 12:42:22 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,23 @@ function startClassification(){
         console.log(error)
     }else{
     console.log(results)
-    
-    
+    document.getElementById("result_label").innerHTML=results[0].label;
+    randomNumberR = Math.floor(Math.random()*255)+1;
+randomNumberG = Math.floor(Math.random()*255)+1;
+randomNumberB = Math.floor(Math.random()*255)+1;
+document.getElementById("h3").style.color = "rgb("+randomNumberR+","+randomNumberG+","+randomNumberB+")";
+document.getElementById("results_label").innerHTML=results[0].label;
+    img  = document.getElementById("animal_image");
+
+    if(results[0].label == "Dog"){
+    img.src="dog.gif";
+    }else if(results[0].label == "Cat"){
+        img.src="cat.gif";
+
+    }else if(results[0].label == "Lion"){
+        img.src="lion.gif";
+    }else if(results[0].label == "Parrot"){
+        img.src="parrot.gif"
     }
     }
+     }
